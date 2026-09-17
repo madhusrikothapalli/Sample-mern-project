ce3138cc49085b562e6a6bd16f1ff99b911af0a0
madhusrikothapalli <kothapallimadhusri@gmail.com>
2026-09-17 10:30:56 +0530

    third commit

--- routes/emp_route.js
+++ routes/emp_route.js
@@ -1,7 +1,11 @@
 let express=require('express');
 let router=express.Router();
-router.post("/register",(req,res)=>{
-    res.send("Register  route");
+let {users}=require('../models/users');
+router.post("/register",async (req,res)=>{
+    console.log(req.body);
+    let newuser=users(req.body);
+    let result=await newuser.save();
+    res.send(result);
 });
 router.post("/login",(req,res)=>{
     res.send("Login route");
