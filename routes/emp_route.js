f6e11d10e8d62ef263130df5c5bc28ba168a589e
madhusrikothapalli <kothapallimadhusri@gmail.com>
2026-09-21 13:52:46 +0530

    third commit

--- routes/emp_route.js
+++ routes/emp_route.js
@@ -1,14 +1,28 @@
 let express=require('express');
 let router=express.Router();
+let bcrypt=require('bcrypt');
 let {users}=require('../models/users');
 router.post("/register",async (req,res)=>{
     console.log(req.body);
+    req.body.password=await bcrypt.hash(req.body.password,10);
     let newuser=users(req.body);
     let result=await newuser.save();
     res.send(result);
 });
-router.post("/login",(req,res)=>{
-    res.send("Login route");
+router.post("/login",async(req,res)=>{
+    let result=await users.findOne({email:req.body.email})
+   
+    if(result){
+        let matchpass=await bcrypt.compare(req.body.password,result.password);
+        if(matchpass){
+            res.send("login successfully");
+        }else{
+            res.send("login failed");
+        }
+    }else{
+        res.send("user not found");
+    }
+   
 });
 router.get("/viewtasks",(req,res)=>{
     res.send("View tasks route");
--- routes/hr_route.js
+++ routes/hr_route.js
@@ -1,6 +1,10 @@
 let express=require('express');
-
 let router=express.Router();
+let {users}=require('../models/users');
+router.get("/viewemployees",async(req,res)=>{
+    let result=await users.find();
+    res.send(result);
+})
 router.get("/viewemployees",(req,res)=>{
     res.send("View employees route");
 });
